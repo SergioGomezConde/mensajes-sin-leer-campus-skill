89a923761cebd178c5c6e85d14f6ebbd9edb4868
Sergio Gómez Conde <58828599+SergioGomezConde@users.noreply.github.com>
2022-06-28 13:59:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -79,7 +79,7 @@ class MensajesSinLeerCampus(MycroftSkill):
             'privados_sin_leer': privados_sin_leer
         })
         
-        with open(ficheroJSON, '+') as ficheroDatos:
+        with open(ficheroJSON, 'a') as ficheroDatos:
                 json.dump(informacion, ficheroDatos, indent=4)
         ficheroDatos.close()
 
