3420c1f2cf05a71f8962ee9dbdf7d6e8f856372c
Sergio Gómez Conde <58828599+SergioGomezConde@users.noreply.github.com>
2022-06-28 14:07:39 +0200

    Solo lectura desde JSON

--- __init__.py
+++ __init__.py
@@ -15,21 +15,21 @@ class MensajesSinLeerCampus(MycroftSkill):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('campus.leer.sin.mensajes.intent')
-    def handle_campus_leer_sin_mensajes(self, message)
+    def handle_campus_leer_sin_mensajes(self, message):
 
-    # Lectura de la informacion del fichero JSON
-    with open(ficheroJSON) as ficheroMensajes:
-        data = json.load(ficheroMensajes)
-        if(data['mensajes'][0]['totales_sin_leer'] == "0"):
-            self.speak("No tienes ningun mensaje sin leer")
+        # Lectura de la informacion del fichero JSON
+        with open(ficheroJSON) as ficheroMensajes:
+            data = json.load(ficheroMensajes)
+            if(data['mensajes'][0]['totales_sin_leer'] == "0"):
+                self.speak("No tienes ningun mensaje sin leer")
 
-        elif(data['mensajes'][0]['totales_sin_leer'] == "1"):
-            self.speak("Tienes un mensaje sin leer")
+            elif(data['mensajes'][0]['totales_sin_leer'] == "1"):
+                self.speak("Tienes un mensaje sin leer")
 
-        else:
-            self.speak("Tienes " + data['mensajes'][0]['totales_sin_leer'] + " mensajes sin leer")
+            else:
+                self.speak("Tienes " + data['mensajes'][0]['totales_sin_leer'] + " mensajes sin leer")
 
-    ficheroMensajes.close()
+        ficheroMensajes.close()
                 
         # # Respuesta con el numero de mensajes totales sin leer
         # if(numeroMensajes == "0"):
