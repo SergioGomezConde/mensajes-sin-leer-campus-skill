e38682f456b01567b7762325cc2dab86de9c1691
Sergio Gómez Conde <58828599+SergioGomezConde@users.noreply.github.com>
2022-06-28 14:20:21 +0200

    Actualización ruta fichero JSON

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ from mycroft import MycroftSkill, intent_file_handler
 # from selenium.webdriver.common.by import By
 
 # Fichero JSON donde almacenar la informacion
-ficheroJSON = "/home/serggom/data.json"
+ficheroJSON = "/home/serggom/scraping/data.json"
 # informacion = {'asignaturas': [], 'usuario': [], 'eventos': [], 'siguiente_evento': [], 'mensajes': []}
 
 
