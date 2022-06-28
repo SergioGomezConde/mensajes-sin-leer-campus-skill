de23ad64af30206c3ce32af078db1f3f2150bc85
Sergio Gómez Conde <58828599+SergioGomezConde@users.noreply.github.com>
2022-06-28 14:06:55 +0200

    Solo lectura desde JSON

--- __init__.py
+++ __init__.py
@@ -10,67 +10,26 @@ from selenium.webdriver.common.by import By
 # Fichero JSON donde almacenar la informacion
 ficheroJSON = "/home/serggom/data.json"
 
-def inicio_sesion(self):
-    # Datos de acceso fijos
-    usuario = 'e71180769r'
-    contrasena = 'p5irZ9Jm4@9C#6WUaE!z9%@V'
-
-    # Modo headless
-    options = Options()
-    options.headless = True
-    options.add_argument("--windows-size=1920,1200")
-
-    self.speak("Buscando la informacion...")
-
-    # Acceso a pagina
-    driver = webdriver.Chrome(options=options)
-    driver.get('https://campusvirtual.uva.es/login/index.php')
-
-    # Inicio de sesion
-    driver.find_element(by=By.NAME, value='adAS_username').send_keys(usuario)
-    driver.find_element(
-        by=By.NAME, value='adAS_password').send_keys(contrasena)
-    driver.find_element(by=By.NAME, value='adAS_submit').click()
-
-    # Aceptar cookies
-    driver.implicitly_wait(10)
-    driver.find_element(
-        by=By.XPATH, value='/html/body/div[1]/div/a[1]').click()
-
-    return driver
-
-
 class MensajesSinLeerCampus(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('campus.leer.sin.mensajes.intent')
-    def handle_campus_leer_sin_mensajes(self, message):
-        driver = inicio_sesion(self)
+    def handle_campus_leer_sin_mensajes(self, message)
 
-        # Acceso a la seccion de mensajes
-        driver.get('https://campusvirtual.uva.es/message/index.php')
+    # Lectura de la informacion del fichero JSON
+    with open(ficheroJSON) as ficheroMensajes:
+        data = json.load(ficheroMensajes)
+        if(data['mensajes'][0]['totales_sin_leer'] == "0"):
+            self.speak("No tienes ningun mensaje sin leer")
 
-        # Obtencion del numero de mensajes totales sin leer
-        time.sleep(10)
-        numeroMensajes = str(driver.find_element(
-            by=By.XPATH, value='/html/body/nav/ul[2]/div[3]/a/div').get_attribute('aria-label').split(' ')[1])
+        elif(data['mensajes'][0]['totales_sin_leer'] == "1"):
+            self.speak("Tienes un mensaje sin leer")
 
-        time.sleep(10)
+        else:
+            self.speak("Tienes " + data['mensajes'][0]['totales_sin_leer'] + " mensajes sin leer")
 
-        # Lectura de la informacion del fichero JSON
-        with open(ficheroJSON) as ficheroMensajes:
-            data = json.load(ficheroMensajes)
-            if(data['mensajes'][0]['totales_sin_leer'] == "0"):
-                self.speak("No tienes ningun mensaje sin leer")
-
-            elif(data['mensajes'][0]['totales_sin_leer'] == "1"):
-                self.speak("Tienes un mensaje sin leer")
-
-            else:
-                self.speak("Tienes " + data['mensajes'][0]['totales_sin_leer'] + " mensajes sin leer")
-
-        ficheroMensajes.close()
+    ficheroMensajes.close()
                 
         # # Respuesta con el numero de mensajes totales sin leer
         # if(numeroMensajes == "0"):
