cd5447b8b762168f6d68a4cb26bb12a3baf584ca
Sergio Gómez Conde <58828599+SergioGomezConde@users.noreply.github.com>
2022-06-28 14:05:18 +0200

    Lectura desde JSON

--- __init__.py
+++ __init__.py
@@ -9,8 +9,6 @@ from selenium.webdriver.common.by import By
 
 # Fichero JSON donde almacenar la informacion
 ficheroJSON = "/home/serggom/data.json"
-informacion = {'asignaturas': [], 'usuario': [], 'eventos': [], 'siguiente_evento': [], 'mensajes': []}
-
 
 def inicio_sesion(self):
     # Datos de acceso fijos
@@ -59,29 +57,6 @@ class MensajesSinLeerCampus(MycroftSkill):
             by=By.XPATH, value='/html/body/nav/ul[2]/div[3]/a/div').get_attribute('aria-label').split(' ')[1])
 
         time.sleep(10)
-        
-        # Obtencion de los distintos numeros de mensajes
-        total_destacados = str(driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div[1]/div[1]/button/small').get_attribute('aria-label').split(' ')[0])
-        destacados_sin_leer = str(driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div[1]/div[1]/button/span[5]').get_attribute('aria-label').split(' ')[1])
-        total_grupo = str(driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/button/small').get_attribute('aria-label').split(' ')[0])
-        grupo_sin_leer = str(driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/button/span[5]').get_attribute('aria-label').split(' ')[1])
-        total_privados = str(driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div[3]/div[1]/button/small').get_attribute('aria-label').split(' ')[0])
-        privados_sin_leer = str(driver.find_element(by=By.XPATH, value='/html/body/div[4]/div[2]/div/div/section/div/div/div/div/div/div/div[1]/div/div[2]/div[1]/div/div[3]/div[1]/button/span[5]').get_attribute('aria-label').split(' ')[1])
-        
-        # Almacenamiento de la informacion en el fichero JSON
-        informacion['mensajes'].append({
-            'totales_sin_leer': str(numeroMensajes),
-            'total_destacados': total_destacados,
-            'destacados_sin_leer': destacados_sin_leer,
-            'total_grupo': total_grupo,
-            'grupo_sin_leer': grupo_sin_leer,
-            'total_privados': total_privados,
-            'privados_sin_leer': privados_sin_leer
-        })
-        
-        with open(ficheroJSON, 'w') as ficheroDatos:
-                json.dump(informacion, ficheroDatos, indent=4)
-        ficheroDatos.close()
 
         # Lectura de la informacion del fichero JSON
         with open(ficheroJSON) as ficheroMensajes:
